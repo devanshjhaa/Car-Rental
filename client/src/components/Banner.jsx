c979c4357c8b2c6b27dbdebcd3f456e0af4ef1af
devanshjhaa <devanshjhaa@gmail.com>
2025-07-06 22:43:34 +0530

    v3.1

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -23,7 +23,7 @@ const Banner = () => {
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          className="px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer"
+          className="px-6 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded-lg text-sm mt-4 cursor-pointer"
         >
           List your Car
         </motion.button>
